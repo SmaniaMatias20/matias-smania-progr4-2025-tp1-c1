7e8d4da862cce6ef8935bf4bd25be536d957e72d
Matias <matiasezequielsmania@gmail.com>
2025-04-16 20:44:27 -0300

    RouterLink

--- Front/src/app/components/navbar/navbar.component.ts
+++ Front/src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
--- Front/src/app/pages/auth/auth.component.ts
+++ Front/src/app/pages/auth/auth.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
-  imports: [LoginComponent, RegisterComponent],
+  imports: [LoginComponent, RegisterComponent, RouterLink],
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css']
 })
