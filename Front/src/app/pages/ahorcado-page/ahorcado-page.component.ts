bf6e756f2036f8d62aa3add3868aa068d665ed19
Matias <matiasezequielsmania@gmail.com>
2025-04-19 01:47:24 -0300

    nuevas paginas

---
+++ Front/src/app/pages/ahorcado-page/ahorcado-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ahorcado-page',
+  imports: [],
+  templateUrl: './ahorcado-page.component.html',
+  styleUrl: './ahorcado-page.component.css'
+})
+export class AhorcadoPageComponent {
+
+}
---
+++ Front/src/app/pages/preguntados-page/preguntados-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-preguntados-page',
+  imports: [],
+  templateUrl: './preguntados-page.component.html',
+  styleUrl: './preguntados-page.component.css'
+})
+export class PreguntadosPageComponent {
+
+}
