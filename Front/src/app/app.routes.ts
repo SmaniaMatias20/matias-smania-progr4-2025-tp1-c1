93764bf959a70dc6a76c39d62dbb2605dc7444b5
Matias <matiasezequielsmania@gmail.com>
2025-04-19 01:58:37 -0300

    arregle el boton logout

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -1,13 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { AuthComponent } from './pages/auth/auth.component';  // Importamos el AuthComponent
+import { AuthComponent } from './pages/auth/auth.component';
 import { AboutComponent } from './pages/about/about.component';
+import { ChatRoomComponent } from './pages/chat-room/chat-room.component';
 
 export const routes: Routes = [
-    { path: 'login', component: AuthComponent }, // Ruta para login
-    { path: 'home', component: HomeComponent },   // Ruta para home
-    { path: 'about', component: AboutComponent },   // Ruta para home
-    { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirige a home al iniciar
+    { path: 'login', component: AuthComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'chat', component: ChatRoomComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: "**", redirectTo: "error", pathMatch: "full" },
 ];
 
