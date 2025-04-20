4e9ec2f249e1bcfb7f1f875163402f9acf1a080d
Matias <matiasezequielsmania@gmail.com>
2025-04-20 20:33:41 -0300

    .

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AuthComponent } from './pages/auth/auth.component';
 import { AboutComponent } from './pages/about/about.component';
+import { ResultsComponent } from './pages/results/results.component';
 import { ChatRoomComponent } from './pages/chat-room/chat-room.component';
 import { MayorMenorPageComponent } from './pages/mayor-menor-page/mayor-menor-page.component';
 import { AhorcadoPageComponent } from './pages/ahorcado-page/ahorcado-page.component';
@@ -11,6 +12,7 @@ export const routes: Routes = [
     { path: 'login', component: AuthComponent },
     { path: 'home', component: HomeComponent },
     { path: 'about', component: AboutComponent },
+    { path: 'results', component: ResultsComponent },
     { path: 'chat', component: ChatRoomComponent },
     { path: 'mayor-menor', component: MayorMenorPageComponent },
     { path: 'ahorcado', component: AhorcadoPageComponent },
--- Front/src/app/pages/home/home.component.ts
+++ Front/src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, Signal } from '@angular/core';
 import { CardGameComponent } from './components/card-game/card-game.component';
 import { AuthService } from '../../services/auth/auth.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -11,13 +12,19 @@ import { AuthService } from '../../services/auth/auth.service';
 })
 export class HomeComponent {
   user!: Signal<any>;
-  ahorcadoImage = 'assets/images/ahorcado/ahorcado.jpg';
-  preguntadosImage = 'assets/images/preguntados/preguntados.png';
-  mayorMenorImage = 'assets/images/mayor-menor/mayor-menor.jpg';
+  games: any[] = [];
 
   constructor(
-    private authService: AuthService
+    private authService: AuthService,
+    private http: HttpClient
   ) {
     this.user = this.authService.user;
+  }
+
+
+  ngOnInit(): void {
+    this.http.get<any[]>('assets/data/games.json').subscribe((data) => {
+      this.games = data;
+    });
   }
 }
