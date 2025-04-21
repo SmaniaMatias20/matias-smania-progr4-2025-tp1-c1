6012a20ae5a0de71aedaadbe302b710d45d5d41d
Matias <matiasezequielsmania@gmail.com>
2025-04-20 21:51:38 -0300

    tabla generica

---
+++ Front/src/app/pages/results/components/results-table/results-table.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-results-table',
+  templateUrl: './results-table.component.html',
+  styleUrls: ['./results-table.component.css']
+})
+export class ResultsTableComponent {
+  @Input() columns: { field: string, label: string }[] = [];
+  @Input() data: any[] = [];
+}
--- Front/src/app/pages/results/results.component.ts
+++ Front/src/app/pages/results/results.component.ts
@@ -1,11 +1,46 @@
 import { Component } from '@angular/core';
+import { ResultsTableComponent } from './components/results-table/results-table.component';
 
 @Component({
   selector: 'app-results',
-  imports: [],
+  imports: [ResultsTableComponent],
   templateUrl: './results.component.html',
   styleUrl: './results.component.css'
 })
 export class ResultsComponent {
+  currentPage: number = 1;
+  totalPages: number = 4;
+
+  columns = [
+    { field: 'name', label: 'Nombre' },
+    { field: 'score', label: 'Puntuación' },
+    { field: 'date', label: 'Fecha' }
+  ];
+
+  scores = [
+    { name: 'Juan', score: 95, date: '2024-04-20' },
+    { name: 'Ana', score: 88, date: '2024-04-19' },
+    { name: 'Luis', score: 72, date: '2024-04-18' },
+    { name: 'Juan', score: 95, date: '2024-04-20' },
+    { name: 'Ana', score: 88, date: '2024-04-19' },
+    { name: 'Juan', score: 95, date: '2024-04-20' },
+    { name: 'Ana', score: 88, date: '2024-04-19' },
+    { name: 'Luis', score: 72, date: '2024-04-18' },
+    { name: 'Juan', score: 95, date: '2024-04-20' },
+    { name: 'Ana', score: 88, date: '2024-04-19' },
+    { name: 'Luis', score: 72, date: '2024-04-18' }
+  ];
+
+
+  changePage(direction: number): void {
+    this.currentPage += direction;
+
+    if (this.currentPage < 1) {
+      this.currentPage = 1;
+    } else if (this.currentPage > this.totalPages) {
+      this.currentPage = this.totalPages;
+    }
+  }
+
 
 }
