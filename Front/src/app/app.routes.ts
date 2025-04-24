19f89d715fdc197c7fa929e3174150d9184004cf
Matias <matiasezequielsmania@gmail.com>
2025-04-24 19:38:24 -0300

    lazy loading

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -1,22 +1,53 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { AuthComponent } from './pages/auth/auth.component';
-import { AboutComponent } from './pages/about/about.component';
-import { ResultsComponent } from './pages/results/results.component';
-import { ChatRoomComponent } from './pages/chat-room/chat-room.component';
-import { MayorMenorPageComponent } from './pages/mayor-menor-page/mayor-menor-page.component';
-import { AhorcadoPageComponent } from './pages/ahorcado-page/ahorcado-page.component';
-import { PreguntadosPageComponent } from './pages/preguntados-page/preguntados-page.component';
 
 export const routes: Routes = [
-    { path: 'login', component: AuthComponent },
-    { path: 'home', component: HomeComponent },
-    { path: 'about', component: AboutComponent },
-    { path: 'results', component: ResultsComponent },
-    { path: 'chat', component: ChatRoomComponent },
-    { path: 'mayor-menor', component: MayorMenorPageComponent },
-    { path: 'ahorcado', component: AhorcadoPageComponent },
-    { path: 'preguntados', component: PreguntadosPageComponent },
+    {
+        path: 'auth',
+        loadComponent: () =>
+            import('./pages/auth/auth.component').then((m) => m.AuthComponent),
+    },
+    {
+        path: 'home',
+        loadComponent: () =>
+            import('./pages/home/home.component').then((m) => m.HomeComponent),
+    },
+    {
+        path: 'about',
+        loadComponent: () =>
+            import('./pages/about/about.component').then((m) => m.AboutComponent),
+    },
+    {
+        path: 'results',
+        loadComponent: () =>
+            import('./pages/results/results.component').then((m) => m.ResultsComponent),
+    },
+    {
+        path: 'chat',
+        loadComponent: () =>
+            import('./pages/chat-room/chat-room.component').then((m) => m.ChatRoomComponent),
+    },
+    {
+        path: 'mayor-menor',
+        loadComponent: () =>
+            import('./pages/mayor-menor-page/mayor-menor-page.component').then(
+                (m) => m.MayorMenorPageComponent
+            ),
+    },
+    {
+        path: 'ahorcado',
+        loadComponent: () =>
+            import('./pages/ahorcado-page/ahorcado-page.component').then(
+                (m) => m.AhorcadoPageComponent
+            ),
+    },
+    {
+        path: 'preguntados',
+        loadComponent: () =>
+            import('./pages/preguntados-page/preguntados-page.component').then(
+                (m) => m.PreguntadosPageComponent
+            ),
+    },
+    { path: '**', redirectTo: 'home' },
 ];
 
 
