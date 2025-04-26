0a3f7c91f68d794ec6bd5a3b2c57d1632ea44bd0
Matias <matiasezequielsmania@gmail.com>
2025-04-26 12:20:23 -0300

    .

--- Front/src/app/pages/ahorcado-page/ahorcado-page.component.ts
+++ Front/src/app/pages/ahorcado-page/ahorcado-page.component.ts
@@ -7,7 +7,6 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 })
 export class AhorcadoPageComponent implements OnInit, OnDestroy {
 
-  // Letras del juego
   letters: string[] = [
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
     'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
@@ -18,16 +17,13 @@ export class AhorcadoPageComponent implements OnInit, OnDestroy {
     ['M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
   ];
 
-  // Temporizador de 3:00
   time: string = '03:00';
   private timerInterval: any;
 
-  // Iniciar temporizador
   ngOnInit() {
     this.startTimer();
   }
 
-  // Parar temporizador al destruir el componente
   ngOnDestroy() {
     if (this.timerInterval) {
       clearInterval(this.timerInterval);
@@ -35,7 +31,7 @@ export class AhorcadoPageComponent implements OnInit, OnDestroy {
   }
 
   startTimer() {
-    let totalSeconds = 180; // 3 minutos = 180 segundos
+    let totalSeconds = 180;
 
     this.timerInterval = setInterval(() => {
       const minutes = Math.floor(totalSeconds / 60);
@@ -46,12 +42,11 @@ export class AhorcadoPageComponent implements OnInit, OnDestroy {
       if (totalSeconds > 0) {
         totalSeconds--;
       } else {
-        clearInterval(this.timerInterval); // Detener el temporizador cuando llegue a 0
+        clearInterval(this.timerInterval);
       }
     }, 1000);
   }
 
-  // Método para agregar ceros a la izquierda
   padZero(num: number): string {
     return num < 10 ? `0${num}` : `${num}`;
   }
