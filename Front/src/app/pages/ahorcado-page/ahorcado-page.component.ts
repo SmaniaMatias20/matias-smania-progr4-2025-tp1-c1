faa2882d0f6b8ca63808fe1065a3cdbeb54de37f
Matias <matiasezequielsmania@gmail.com>
2025-04-26 12:41:18 -0300

    logica al ahorcado services

--- Front/src/app/pages/ahorcado-page/ahorcado-page.component.ts
+++ Front/src/app/pages/ahorcado-page/ahorcado-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AhorcadoService } from '../../services/ahorcado/ahorcado.service';
 
 @Component({
   selector: 'app-ahorcado-page',
@@ -17,37 +18,17 @@ export class AhorcadoPageComponent implements OnInit, OnDestroy {
     ['M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
   ];
 
-  time: string = '03:00';
-  private timerInterval: any;
+  constructor(public ahorcadoService: AhorcadoService) { }
 
   ngOnInit() {
-    this.startTimer();
+    this.ahorcadoService.startTimer();
   }
 
   ngOnDestroy() {
-    if (this.timerInterval) {
-      clearInterval(this.timerInterval);
-    }
+    this.ahorcadoService.stopTimer();
   }
 
-  startTimer() {
-    let totalSeconds = 180;
-
-    this.timerInterval = setInterval(() => {
-      const minutes = Math.floor(totalSeconds / 60);
-      const seconds = totalSeconds % 60;
-
-      this.time = `${this.padZero(minutes)}:${this.padZero(seconds)}`;
-
-      if (totalSeconds > 0) {
-        totalSeconds--;
-      } else {
-        clearInterval(this.timerInterval);
-      }
-    }, 1000);
-  }
-
-  padZero(num: number): string {
-    return num < 10 ? `0${num}` : `${num}`;
+  get time(): string {
+    return this.ahorcadoService.getTime();
   }
 }
--- Front/src/app/services/ahorcado/ahorcado.service.ts
+++ Front/src/app/services/ahorcado/ahorcado.service.ts
@@ -5,5 +5,45 @@ import { Injectable } from '@angular/core';
 })
 export class AhorcadoService {
 
+  private timerInterval: any;
+  private totalSeconds: number = 180;
+  public time: string = '03:00';
+
   constructor() { }
+
+  startTimer(callback?: () => void) {
+    this.totalSeconds = 180;
+    this.updateTimeString();
+
+    this.timerInterval = setInterval(() => {
+      if (this.totalSeconds > 0) {
+        this.totalSeconds--;
+        this.updateTimeString();
+      } else {
+        this.stopTimer();
+        if (callback) callback();
+      }
+    }, 1000);
+  }
+
+  stopTimer() {
+    if (this.timerInterval) {
+      clearInterval(this.timerInterval);
+      this.timerInterval = null;
+    }
+  }
+
+  getTime(): string {
+    return this.time;
+  }
+
+  private updateTimeString() {
+    const minutes = Math.floor(this.totalSeconds / 60);
+    const seconds = this.totalSeconds % 60;
+    this.time = `${this.padZero(minutes)}:${this.padZero(seconds)}`;
+  }
+
+  private padZero(num: number): string {
+    return num < 10 ? `0${num}` : `${num}`;
+  }
 }
