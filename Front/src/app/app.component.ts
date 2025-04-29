c0b9bc4ebf06216b59154c85996cade115a8da43
Matias <matiasezequielsmania@gmail.com>
2025-04-29 19:50:35 -0300

    .

--- Front/src/app/app.component.ts
+++ Front/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +12,16 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  showNavbarAndFooter: boolean = true;
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.showNavbarAndFooter = !event.url.includes('/games');
+    });
+  }
 
 }
