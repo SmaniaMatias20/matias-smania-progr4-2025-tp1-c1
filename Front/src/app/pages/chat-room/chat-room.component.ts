a2af2434543c2eee9c43009ee306f3e883ce7552
Matias <matiasezequielsmania@gmail.com>
2025-05-11 16:59:14 -0300

    comentarios

--- Front/src/app/pages/chat-room/chat-room.component.ts
+++ Front/src/app/pages/chat-room/chat-room.component.ts
@@ -17,8 +17,18 @@ export class ChatRoomComponent implements OnInit {
   currentDate: string | null = null;
   message = '';
 
+  /**
+ * Constructor de la clase ChatRoomComponent.
+ * 
+ * @param {ChatService} chatService - Servicio que maneja la lógica de los mensajes en el chat.
+ * @param {AuthService} authService - Servicio que maneja la autenticación del usuario.
+ */
   constructor(private chatService: ChatService, private authService: AuthService) { }
 
+  /**
+ * Método que se ejecuta cuando el componente se inicializa.
+ * @returns {void} - No retorna ningún valor.
+ */
   async ngOnInit() {
     this.user = await this.authService.getUser();
     this.messages = await this.chatService.getMessages();
@@ -28,6 +38,10 @@ export class ChatRoomComponent implements OnInit {
     });
   }
 
+  /**
+   * Método para enviar un mensaje.
+   * @returns {void} - No retorna ningún valor.
+   */
   async sendMessage() {
     if (this.message.trim()) {
       await this.chatService.sendMessage(this.user.id, this.user.firstname, this.user.lastname, this.message);
@@ -35,10 +49,20 @@ export class ChatRoomComponent implements OnInit {
     }
   }
 
+  /**
+ * Método que verifica si el mensaje pertenece al usuario actual.
+ * @param {any} msg - El mensaje que se verifica.
+ * @returns {boolean} - Retorna true si el mensaje es del usuario actual.
+ */
   isMyMessage(msg: any) {
     return msg.user_id === this.user.id;
   }
 
+  /**
+ * Método para formatear la hora de un mensaje.
+ * @param {string} date - Fecha del mensaje.
+ * @returns {string} - Devuelve la hora y fecha formateada.
+ */
   formatTime(date: string): string {
     const d = new Date(date);
 
@@ -51,6 +75,10 @@ export class ChatRoomComponent implements OnInit {
   }
 
 
+  /**
+   * Método para generar un color aleatorio en formato RGB.
+   * @returns {string} - Retorna un color aleatorio en formato RGB.
+   */
   getRandomColor(): string {
     const r = Math.floor(Math.random() * 256);
     const g = Math.floor(Math.random() * 256);
@@ -58,6 +86,12 @@ export class ChatRoomComponent implements OnInit {
     return `rgb(${r},${g},${b})`;
   }
 
+  /**
+ * Método que obtiene el color asignado a un usuario.
+ * Si no tiene un color asignado, lo genera aleatoriamente.
+ * @param {string} username - El nombre de usuario.
+ * @returns {string} - Retorna el color asignado al usuario.
+ */
   getUserColor(username: string): string {
     if (!this.userColors[username]) {
       this.userColors[username] = this.getRandomColor();
